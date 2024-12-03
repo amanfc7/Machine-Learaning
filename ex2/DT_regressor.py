902f4781291f3bd34991ea3e6347f13bb2d65bf5
Gabriel Haubenburger <haubenburger.gabriel@gmx.at>
2024-12-03 09:13:36 +0100

    fleshed out the tree a bit more

--- ex2/DT_regressor.py
+++ ex2/DT_regressor.py
@@ -15,7 +15,9 @@ class DT_Regressor():
         TODO: something about (pre-)pruning
     """
     def __init__(self, max_depth=-1, split_on_decision=2, compute_split_alg="error_rate"):
-        pass
+        self.max_depth = max_depth
+        self.split_on_decision = split_on_decision
+        self.tree_root = None
     
     
     """
@@ -24,15 +26,27 @@ class DT_Regressor():
     def fit(self, X, y):
         pass
     
-    
-    def predict(self):
-        pass
+    """
+        predicts and returns y for the given X
+    """
+    def predict(self, X):
+        y = [] # TODO: might want a numpy array here instead or pd dataframe?
+        if self.tree_root = None:
+            pass #raise Error or just invalid value?
+        else:
+            for X_i in X:
+                y.append(self.tree_root.get_prediction(X_i))
+            return y
+                
     
     
     class TreeNode():
         
         def __init__(self, num_children=2):
-            pass
+            if num_children > 0:
+                self.children = [None for i in range(num_children)]
+            else:
+                self.children = None
         
         def get_prediction(self, X_i):
             pass
@@ -41,14 +55,16 @@ class DT_Regressor():
         
         """
         test should be a function that takes a vector/list and based on that returns a non-negative integer < num_children
+            test should be set during the training step
         """
         def __init__(self, test, num_children=2):
-            pass
+            super().__init__(num_children=num_children)
+            self.test = test
         
         def get_prediction(self, X_i):
             #use test to get the prediction value of one of the child nodes
             # something like: return self.children[test(X_i)].get_prediction(X_i)
-            pass
+            return self.children[self.test(X_i)].get_prediction(X_i)
         
     class LeafNode(TreeNode):
         
@@ -56,11 +72,12 @@ class DT_Regressor():
             prediction value should either get set to the value of the average of all values contained in this leaf node
         """
         def __init__(self, prediction_value):
-            pass
+            super().__init__(num_children=0)
+            self.prediction_value = prediction_value
         
         def get_prediction(self, X_i):
             #return ´own prediction value
-            pass
+            return self.prediction_value
 
 
 def main():
