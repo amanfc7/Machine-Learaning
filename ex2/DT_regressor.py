b37b01f5641985bb9bf60854a01e00d556261940
Gabriel Haubenburger <haubenburger.gabriel@gmx.at>
2024-12-05 17:03:02 +0100

    finished creating tree structure

--- ex2/DT_regressor.py
+++ ex2/DT_regressor.py
@@ -26,6 +26,7 @@ class DT_Regressor():
     
     """
         trains the model
+        X should be a numpy array of shape(number_of_samples, number_of_features)
     """
     def fit(self, X, y):
         if self.max_depth == 0:
@@ -43,14 +44,28 @@ class DT_Regressor():
                     ## add leaf nodes
                     # pass
                     
+    """
+        X should be a numpy array of shape(number_of_samples, number_of_features)
+        should be able to correctly learn multiple regression values at the same time 
+            (more than one target column)
+    """
     def _fit(self, X, y, i):
-        if i == 0 or False: #TODO: False should be replaced with only one value remaining in box
-            #add leaf nodes
+        if X.ndim == 1: 
+            #only one sample left, we want to predict its y-value(s) 
+            return self.LeafNode(y)
+        elif i == 0:
+            #max depth reached, we want to predict the mean(s) of its y-values 
             return self.LeafNode(np.mean(y,axis=0))
         else:
-            # return an inner node with children set by recursive calls to _fit(), with i-1 and probably adjusted X, y 
+            # return an inner node with children set by recursive calls to _fit(), with i-1 and split X, y 
             #    --> divide X based on whatever splitting criterion I calculate, then pass only the respective parts of X and the corresponding parts of y to the repective recursive calls
-            pass
+            test = self._create_split(X, y)
+            children = [self._fit(X, y, i-1) for j in range(self.split_on_decision)] #TODO: split X, y
+            return self.InnerNode(test, children)
+        
+    def _create_split(self, X, y):
+        return lambda X_i: 0
+        # pass
         
     
     """
@@ -59,7 +74,7 @@ class DT_Regressor():
     def predict(self, X):
         y = [] 
         if self.tree_root == None:
-            pass #raise Error or just invalid value?
+            raise AttributeError("Tree has not been trained. No root set")
         else:
             for X_i in X:
                 y.append(self.tree_root.get_prediction(X_i))
@@ -68,57 +83,55 @@ class DT_Regressor():
     
     class TreeNode():
         
-        def __init__(self, num_children=2):
-            if num_children > 0:
-                self.children = [None for i in range(num_children)]
-            else:
-                self.children = None
+        def __init__(self, children):
+            self.children = children
         
         def get_prediction(self, X_i):
             pass
         
-        def add_child_node(self, node):
-            #prbly add node at index i in self.children, then increase local i by one, raise error if i >= num_children instead
-            # could also try assignment directly and catch TypeError (Child node) or IndexError (Full inner node)
-            #might instead want to add all children at once
-            pass
+        # def add_child_node(self, node):
+        #     #prbly add node at index i in self.children, then increase local i by one, raise error if i >= num_children instead
+        #     # could also try assignment directly and catch TypeError (Child node) or IndexError (Full inner node)
+        #     #might instead want to add all children at once
+        #     pass
         
     class InnerNode(TreeNode):
         
         """
-        test should be a function that takes a vector/list and based on that returns a non-negative integer < num_children
+        test should be a function that takes a vector/list and based on that 
+            returns a non-negative integer < len(children)
             test should be set during the training step
         """
-        def __init__(self, test, num_children=2):
-            super().__init__(num_children=num_children)
+        def __init__(self, test, children):
+            super().__init__(children=children)
             self.test = test
         
         def get_prediction(self, X_i):
             #use test to get the prediction value of one of the child nodes
-            # something like: return self.children[test(X_i)].get_prediction(X_i)
             return self.children[self.test(X_i)].get_prediction(X_i)
         
     class LeafNode(TreeNode):
         
         """
-            prediction value should either get set to the value of the average of all values contained in this leaf node
+            prediction value should be the value of the average 
+                of all values contained in this leaf node or the only value
         """
         def __init__(self, prediction_value):
-            super().__init__(num_children=0)
+            super().__init__(children=None)
             self.prediction_value = prediction_value
         
         def get_prediction(self, X_i):
-            #return ´own prediction value
+            #return own prediction value
             return self.prediction_value
     
 
 
 def main():
-    X = np.array([[1,2], [3, 4]])
+    X = np.array([[1,2, 5], [3, 4,8]])
     y = np.array([0.5,0.6])
     # y = np.array([[0.5,0.6], 
     #               [0.6,0.7]])
-    reg = DT_Regressor(max_depth=0)
+    reg = DT_Regressor(max_depth=1)
     reg.fit(X, y)
     print(reg.predict(X))
     print(reg.predict(X).shape)
