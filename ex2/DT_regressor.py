f65567599a3e73533e8fe4b6601f999d6a41e9a0
Gabriel Haubenburger <haubenburger.gabriel@gmx.at>
2024-12-06 12:27:59 +0100

    finished first implementation of regression tree

--- ex2/DT_regressor.py
+++ ex2/DT_regressor.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import random
 
 #vgl. 03, 04, 06, 07
 
@@ -10,16 +11,36 @@ class DT_Regressor():
     """
     Parameters:
         max_depth: max depth of decision tree. 0 for Zero Rule, 1 for One Rule, -1 for unlimited
-        split_on_decision: how many branches per inner node, min 2
+        ---split_on_decision: how many branches per inner node, min 2 (not used, infeasible)
         compute_split_alg: the algorithm used to compute the best split of the data
             should be one out of "error_rate", "information_gain", "gini_index", "variance_reduction", ???
             TODO: actually, might neeed to different, e.g. means squared error (MSE), mean absolute error (MAE)
+        epsilon: stop early if standard deviation of prediction is smaller than this
         TODO: something about (pre-)pruning
+        random_state: parameter for reproducability for eventual random operations; default=None
     """
-    def __init__(self, max_depth=-1, split_on_decision=2, compute_split_alg="error_rate"):
+    def __init__(self, max_depth=-1, 
+        # split_on_decision=2,
+        compute_split_alg="mse",
+        epsilon=0.001,
+        random_state=None):
         self.max_depth = max_depth
-        self.split_on_decision = split_on_decision
+        # self.split_on_decision = split_on_decision
         self.tree_root = None
+        self.epsilon = epsilon
+        
+        if random_state == None:
+            self.rd = random.Random()
+        else:
+            self.rd = random.Random(random_state)
+            
+        #error measurement for 
+        if compute_split_alg == "mse":
+            self.goodness_test = self._mse
+        elif compute_split_alg == "mae":
+            self.goodness_test = self._mae
+        else:
+            raise AttributeError("invalid goodness commputation method selected")
     
     
 
@@ -52,16 +73,21 @@ class DT_Regressor():
     def _fit(self, X, y, i):
         # if X.ndim == 1: 
         if X.shape[0] == 1:
-            #only one sample left, we want to predict its y-value(s) 
+            #only one sample (row) left, we want to predict its y-value(s) 
             return self.LeafNode(y)
         elif i == 0:
             #max depth reached, we want to predict the mean(s) of its y-values 
+            return self.LeafNode(np.mean(y,axis=0))
+        elif np.max(np.sqrt(self._mse(y.mean(axis=0), y))) < self.epsilon:  
+            #standard deviation for (all) predicted values is smaller than our target value, stop early
+            #so for all X_is the average of the corresponding y_is is not too far from the individual y_is
             return self.LeafNode(np.mean(y,axis=0))
         else:
             # return an inner node with children set by recursive calls to _fit(), with i-1 and split X, y 
             #    --> divide X based on whatever splitting criterion I calculate, then pass only the respective parts of X and the corresponding parts of y to the repective recursive calls
             test = self._create_split(X, y)
-            masks = [[test(X_i) == j for X_i in X] for j in range(self.split_on_decision)] #TODO: could probably make this more efficient by better utilizing numpy
+            # masks = [[test(X_i) == j for X_i in X] for j in range(self.split_on_decision)] #split_on_decision is no longer used, delete this
+            masks = [[test(X_i) == j for X_i in X] for j in range(2)] #TODO: could probably make this more efficient by better utilizing numpy
             # print(X)
             # print(X.ndim)
             # print(X.shape)
@@ -71,13 +97,49 @@ class DT_Regressor():
             return self.InnerNode(test, children)
         
     def _create_split(self, X, y):
-        def test(X_i):  #TODO: make this propa
-            for i in range(1, self.split_on_decision):
-                if X_i[0] > i:
-                    return i
-                else:
-                    return 0
-        return test
+        best_split = lambda X_i: 0
+        best_error = float("inf")
+        
+        #initial brute force attempt
+        for i, sample in enumerate(X):
+            for j, feature in enumerate(sample):
+                #split on feature
+                mask_0 = X[:, j] < feature
+                X_0 = X[mask_0]
+                y_0 = y[mask_0]
+                
+                mask_1 = X[:, j] >= feature
+                X_1 = X[mask_1]
+                y_1 = y[mask_1]
+                
+                if X_0.shape[0] == 0 or X_1.shape[0] == 0:
+                    #we have hit an outermost value, a split is empty so not good
+                    continue
+                    
+                # print(X_0)
+                # print(X_1)
+                
+                error_0 = np.max(self.goodness_test(np.mean(y_0,axis=0), y_0)) #max in case there are multiple predicted values
+                # print(error_0)
+                error_1 = np.max(self.goodness_test(np.mean(y_1,axis=0), y_1))
+                # print(error_1)
+                error = max(error_0, error_1)
+                if error < best_error: #better error
+                    best_split = lambda X_i: 0 if X_i[j] < feature else 1
+                    best_error = error
+                # best_split = test
+                # def test(X_i):  #TODO: make this propa
+                    # # maybe compute avg of y
+                    # # for i in range(1, self.split_on_decision):
+                        # # if X_i[0] > i:
+                            # # return i
+                        # # else:
+                            # # return 0
+                    # if X_i[j] < feature:
+                        # return 0
+                    # else:
+                        # return 
+        return best_split
 
         
     
@@ -90,8 +152,25 @@ class DT_Regressor():
             raise AttributeError("Tree has not been trained. No root set")
         else:
             for X_i in X:
-                y.append(self.tree_root.get_prediction(X_i))
+                prediction = self.tree_root.get_prediction(X_i)
+                if prediction.shape == (1,):
+                    # print(prediction.shape)
+                    prediction = prediction[0] #workaround to negative values being in arrays until I fix it
+                y.append(prediction)
+                # print(prediction)
             return np.array(y)
+    
+    """
+    calculates the mean squared error for two vectors (or two matrices for each of the respective columns)
+    """
+    def _mse(self, prediction_matrix, y):
+        return ((prediction_matrix - y)**2).mean(axis=0)
+        
+    """
+    calculates the mean absolute error for two vectors (or two matrices for each of the respective columns)
+    """
+    def _mae(self, prediction_matrix, y):
+        return (np.abs(prediction_matrix - y)).mean(axis=0)
                 
     
     class TreeNode():
@@ -120,7 +199,7 @@ class DT_Regressor():
             self.test = test
         
         def get_prediction(self, X_i):
-            #use test to get the prediction value of one of the child nodes
+            #use test to get the prediction value of the relevant of the child node
             return self.children[self.test(X_i)].get_prediction(X_i)
         
     class LeafNode(TreeNode):
@@ -141,10 +220,10 @@ class DT_Regressor():
 
 def main():
     X = np.array([[1,2, 5], [3, 4,8]])
-    y = np.array([0.5,0.6])
+    # y = np.array([0.5,0.6])
     # y = np.array([[0.5],[0.6]])
-    # y = np.array([[0.5,0.6], 
-    #               [0.6,0.7]])
+    y = np.array([[0.5,0.6], 
+                  [0.6,0.7]])
     # reg = DT_Regressor(max_depth=0)
     # reg = DT_Regressor(max_depth=1)
     reg = DT_Regressor()
