827b44960aaac93a31136377b8ade55ba71baf8a
Gabriel Haubenburger <haubenburger.gabriel@gmx.at>
2024-12-15 22:54:20 +0100

    presentation

--- ex2/data_set_1_regr.py
+++ ex2/data_set_1_regr.py
@@ -10,6 +10,7 @@ from DT_regressor import DTRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 from sklearn.ensemble import RandomForestRegressor as SKRandomForest
+from sklearn.tree import DecisionTreeRegressor as SKDT
 
 import pandas as pd
 
@@ -28,7 +29,7 @@ def train_model():
     # clf = RandomForestRegressor(use_skl_tree=False, max_samples=100, max_features=20, max_leaf_nodes=20,random_state=random_state) #Training time: 11.509373, Prediction time: 0.386701, R2 score: 0.428260, MSE: 0.028758, MAE: 0.128715
     # clf = RandomForestRegressor(use_skl_tree=False, max_samples=100, max_features=20,random_state=random_state) #Training time: 17.850327, Prediction time: 0.314665, R2 score: 0.436811, MSE: 0.028328, MAE: 0.126197
     # clf = RandomForestRegressor(use_skl_tree=False, max_samples=500, max_features=20,random_state=random_state, n_estimators=20) #Training time: 37.884419, Prediction time: 0.128818, R2 score: 0.454418, MSE: 0.027443, MAE: 0.121389
-    # clf = RandomForestRegressor(use_skl_tree=False, max_samples=500, max_features=30,random_state=random_state, n_estimators=50) #Training time: 105.571297, Prediction time: 0.213839, R2 score: 0.474980, MSE: 0.026408, MAE: 0.118264
+    clf = RandomForestRegressor(use_skl_tree=False, max_samples=500, max_features=30,random_state=random_state, n_estimators=50) #Training time: 105.571297, Prediction time: 0.213839, R2 score: 0.474980, MSE: 0.026408, MAE: 0.118264
 
     # clf = RandomForestRegressor(use_skl_tree=True, max_samples=100, max_features=20, max_leaf_nodes=20,random_state=random_state) #Training time: 0.147338, Prediction time: 0.012817, R2 score: 0.423249, MSE: 0.029010, MAE: 0.129396
     # clf = RandomForestRegressor(use_skl_tree=True, max_samples=100, max_features=20,random_state=random_state) # Training time: 0.159919, Prediction time: 0.018211, R2 score: 0.449597, MSE: 0.027685, MAE: 0.125419
@@ -50,9 +51,11 @@ def train_model():
     
     # t0= time.time()
     # # clf = DTRegressor(splitter='random',  max_leaf_nodes=20, verbose=True, random_state=random_state)
-    # clf = DTRegressor(splitter='random', verbose=True, random_state=random_state)
-    # clf.fit(X_train, y_train) 
-    # print("Training time: %f" % (time.time() - t0))
+    # # clf = DTRegressor(splitter='random', verbose=True, random_state=random_state) # Training time: 26.373381, Prediction time: 0.007716, R2 score: 0.070270, MSE: 0.046765, MAE: 0.152530
+    # clf = SKDT(splitter='random', random_state=random_state) # Training time: 0.022784, Prediction time: 0.001053, R2 score: 0.066601, MSE: 0.046950, MAE: 0.152716
+
+    clf.fit(X_train, y_train) 
+    print("Training time: %f" % (time.time() - t0))
     
     
     #some quick evaluation
