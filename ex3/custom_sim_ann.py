f0417ea4abcf8905ce0d8a7c0ce8b7d1ab4ad480
Gabriel Haubenburger <haubenburger.gabriel@gmx.at>
2025-02-24 15:48:02 +0100

    mostly typos

--- ex3/custom_sim_ann.py
+++ ex3/custom_sim_ann.py
@@ -15,50 +15,45 @@ from data_sets_util import load_ds
 
 
 
-def optimze(X_train, y_train, X_test, y_test, init_T=1000):
+def optimze(X_train, y_train, X_test, y_test, init_T=1000, rng_seed=None):
+    rng = np.random.default_rng(rng_seed)
     start_time = time.time()
     curr_best = None
     curr_best_score = 0.
-    T = init_T
-    t = 0
     
     classifier_0 =  [
         MLPClassifier,
         { # hyperparameter dict
             "activation": ('identity', 'logistic', 'tanh', 'relu'),
             "solver": ('lbfgs', 'sgd', 'adam'),
-            "hidden_layer_sizes": ((15,2), (100,), (15,15), (20,3), (50, 50), (20, 20, 20), (100,100,100)),
-            "alpha": np.logspace(-10, 4, 15),
+            "hidden_layer_sizes": ((15,2), (100,), (15,15), (20,3), (50, 50), (20, 20, 20), (100,100,100)), # --> maybe make that 'continious'
+            "alpha": np.logspace(-10, 4, 15), # --> maybe make that 'continious'
             # "max_iter": (200, 300),
         }
     ]
-    # classifier_0_hp_array = [ # for MLP
-    #     ('identity', 'logistic', 'tanh', 'relu'), #"activation": 
-    #     ('lbfgs', 'sgd', 'adam'), # "solver": 
-    #     ((15,2), (100,), (15,15), (20,3), (50, 50), (20, 20, 20), (100,100,100)), # "hidden_layer_sizes": --> maybe make that 'continious'
-    #     np.logspace(-10, 4, 15), # "alpha": --> continous?
-    #     ]
-    #TODO
+
+    #TODO: second classifier
     classifier_1 =  [
         RandomForestClassifier,
         { # hyperparameter dict
             
         }
     ]
+    #TODO: third classifier
     classifier_2 =  [
         RandomForestClassifier,
         { # hyperparameter dict
             
         }
     ]
-    #TODO
+    #TODO: fourth classifier
     classifier_3 =  [
         RandomForestClassifier,
         { # hyperparameter dict
             
         }
     ]
-    #TODO
+    #TODO: fifth classifier
     classifier_4 =  [
         RandomForestClassifier,
         { # hyperparameter dict
@@ -67,37 +62,45 @@ def optimze(X_train, y_train, X_test, y_test, init_T=1000):
     ]
 
     all_classifiers_array = [classifier_0, classifier_1, classifier_2, classifier_3, classifier_4]
-    
-    # select random initial solution
-    #TODO: seems fine, check again
     max_hp_number = max([len(classifier[1].keys()) for classifier in all_classifiers_array])
-    init_solution_vect = np.random.rand(1+max_hp_number)
     
+    # time stamp = 0
+    t = 0
+    # Intialize T #(initial temperature)
+    T = init_T 
+           
+
+    # select random initial current solution v_c
+    #TODO: seems fine, check again
+    init_solution_vect = rng.random(1+max_hp_number)
     clf = solution_vect_to_clf(init_solution_vect, all_classifiers_array)
+    # Evalutate v_c
     curr_best_score = eval_solution(clf, X_train, y_train, X_test, y_test)
     curr_score = curr_best_score
 
     current_solution = init_solution_vect
     current_best = init_solution_vect
 
-    # now loop until stop
-    # stopping criterion is that one hour has passed (and/or perfect accuracy achieved?)
-
-
+    # now loop until th halting criterion is reached
     while (halting_criterion(start_time)): #one hour has not yet passed
         i = 0
-        while (termination_codition(i)):
+        # loop until the termination condition for the current time step has been reached
+        while (termination_condition(i)):
             i = i + 1
+            # select a new solution v_n in the neighborhood of v_c ...
             new_solution = select_neighbor(current_solution, all_classifiers_array, T)
+            # ... and evalute it
             new_score = eval_solution(solution_vect_to_clf(new_solution, all_classifiers_array), X_train, y_train, X_test, y_test)
+            # if it's better than v_c, update v_c
             if curr_score < new_score:
                 current_solution = new_solution
                 curr_score = new_score
                 if curr_best_score < curr_score:
                     current_best = current_solution
                     curr_best_score = curr_score
+            # even if it's not better, update with a certain probability
             else:
-                if np.random.rand(1) < np.exp((new_score - curr_score) / T):
+                if rng.random() < np.exp((new_score - curr_score) / T):
                     current_solution = new_solution
                     curr_score = new_score
 
@@ -106,31 +109,18 @@ def optimze(X_train, y_train, X_test, y_test, init_T=1000):
         t = t + 1
         # print(t)
 
-    
+    #TODO:
+    # we want to return/do something with the best found solution
+    print(current_best)
+    print(curr_best_score)
 
-#     Prozeduresimulated annealing
-# begin
-#   t=0
-#   Intialize T #(initial temperature)
-#   select a current solution vc at random
-#   evaluate vc
-#   repeat
-#       repeat
-#           select a new solution vn in the neighborhood of vc
-#           if eval(vc) < eval(vn) then vc=vn
-#           else if random[0,1) e^((eval(v_n)-eval(v_c))/T)then vc=vn
-#       until(termination-condition)
-#       T=g(T,t) #(do cooling)
-#       t=t+1
-#   until(halting-criterion)
-# end
 
-#idea: encode algs in vectors of length 1+[max hyperparameters to tone over all algs], where the first component indicates the alg and the others indicate teh hyperparameter. If chosen alg has less than max hyperparameters, ignore excess 
+#idea: encode algs in vectors of length 1+[max hyperparameters to tone over all algs], where the first component indicates the alg and the others indicate the hyperparameter. If chosen alg has less than max hyperparameters, ignore excess 
 #       if some hyperparameters have higher ranges, will probably need to round for lower --> just pick values from 0 to 1 and then scale to range!
 
 # should temperature restrict the size of the neighborhood or just affect the chance with which a worse solution gets kept? (easier) 
 
-
+#TODO: implement/test different neighborhood selectors
 """
     Returns a solution in the neighborhood of the current solution
 """
@@ -146,10 +136,11 @@ def cool_down(T, t):
     return T * reduction_factor
     # return T - 1
 
+#TODO: implement/test different conditions
 """
     Termination condition for one time step
 """
-def termination_codition(i):
+def termination_condition(i):
     return i < 10
 
 """
@@ -162,7 +153,7 @@ def halting_criterion(start_time):
     return False
 
 """
-    Evaluates the goodness of a solution
+    Trains the classifier on the provided data and evalutes it
 """
 def eval_solution(solution_clf, X_train, y_train, X_test, y_test):
     solution_clf.fit(X_train, y_train)
@@ -202,11 +193,8 @@ def main():
         except ValueError:
             print("Could not load specified data set. Ensure the parameter is on of the integers 1-4")
 
-
-    (X_train, y_train, X_test, y_test) = load_ds(ds_to_load)
+    X_train, y_train, X_test, y_test = load_ds(ds_to_load)
     optimze(X_train, y_train, X_test, y_test)
-
-
 
 
 if __name__ == '__main__':
